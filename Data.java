9b7a50fdecb8a84e482678e3f7c2c533fc1b6237
ahmedalikeerio <49383934+ahmedalikeerio@users.noreply.github.com>
2019-04-09 00:56:34 +0500

    upload lab1 tasks
    Use Github web GUI

---
+++ Data.java
@@ -0,0 +1,13 @@
+public class Data{
+	public static void main(String args[]){
+String Name="Ahmed Ali";
+int marks=90;
+float per=(90*100)/100;
+char c='A';
+String Status="Pass";
+int roll=56;
+
+System.out.println("Name: "+Name+"\nRoll no: 18SW"+roll+"\nMarks in last semester: "+marks+"\nPercentage: "+per+"%"+"\nGrade: "+c);
+
+	}
+}
---
+++ Task.java
@@ -0,0 +1,9 @@
+public class Task{
+public static void main(String[]args){
+	double d=50.25;
+	long l=(int)50.25;
+	double d1=d-l;
+	System.out.println("Actual number is: "+d+"\nIntegral part is: "+l+"\nFractionsl part is: "+d1);
+}
+
+}
